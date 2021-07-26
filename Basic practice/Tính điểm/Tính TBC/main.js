914b1ba18c4147c98f44b7cd90a18e875e8615ea
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-26 10:16:05 +0700

    Tinh TBC

---
+++ Basic practice/Tính điểm/Tính TBC/main.js
@@ -0,0 +1,17 @@
+function tinhtbc(x, y, z, m, n, p) {
+    var ketqua = (x + y + z + m + n + p)/6;
+    return ketqua;
+}
+
+console.log(tinhtbc(2, 5, 6, 7, 3, 7));
+
+if (tinhtbc > 5) {
+    console.log('TBC lớn hơn 5');
+} else if (tinhtbc = 5){
+    console.log('TBC bằng 5');
+} else{
+    console.log('TBC bé hơn 5');
+};
+
+
+
