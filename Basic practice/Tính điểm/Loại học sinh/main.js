2a2f483b33614b1f5d39592d33d7a6c14f85b450
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-26 10:20:15 +0700

    Xep loai hoc sinh

---
+++ Basic practice/Tính điểm/Loại học sinh/main.js
@@ -0,0 +1,16 @@
+var diem = 9;
+if (diem >= 8 && diem <= 10) {
+    console.log('Học sinh giỏi');
+} else if (diem >= 6.5 && diem < 8) {
+    console.log('Học sinh tiên tiến');
+} else if (diem >=2 && diem < 6.5) {
+    console.log('Học sinh trung bình');
+} else if (diem >= 0 && diem < 2) {
+    console.log('Học sinh yếu');
+} else{
+    console.log('Điểm không hợp lệ, vui lòng kiểm tra lại!');
+}
+
+
+
+
