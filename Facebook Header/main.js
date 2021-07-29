f368284fd12fbc16df3c3718db8206f7c75f7ca9
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-29 17:10:08 +0700

    Hieu ung cho nhieu nut o Header Facebook

--- Facebook Header/main.js
+++ Facebook Header/main.js
@@ -1,15 +1,38 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var tamgiac = document.getElementsByClassName("header-icon");
-    var tamgiac = tamgiac[0];
-    var danhsach = document.getElementsByClassName("header__list");
-    var danhsach = danhsach[0];
+document.addEventListener("DOMContentLoaded", function() {
+    var nut = document.getElementsByClassName('header-icon');
+    var nd = document.getElementsByClassName('header__list')
+    for (let i = 0; i < nut.length; i++) {
+        nut[i].onclick = function() {
+            if (this.classList[1] == 'header-icon--click' ) {
+                this.classList.remove('header-icon--click')
 
-    tamgiac.onclick = function() {
-        this.classList.toggle('header-icon--click');  
-        danhsach.classList.toggle('header__list--click');
+                var noidunghienra = this.getAttribute('data-visible');
+                var phantuhienra = document.getElementById(noidunghienra);
+                
+                phantuhienra.classList.toggle('header__list--click');
+            }
+            else{
+                for (let j = 0; j < nut.length; j++) {
+                    nut[j].classList.remove('header-icon--click');
+                    nut[j].classList.remove('header__list--click');
+                }
+                nut[i].classList.toggle('header-icon--click');
+                nut[i].classList.toggle('header__list--click');
+
+                var noidunghienra = this.getAttribute('data-visible');
+                var phantuhienra = document.getElementById(noidunghienra);
+                for (let k = 0; k < nd.length; k++) {
+                    nd[k].classList.remove('header__list--click');;
+                }
+                phantuhienra.classList.toggle('header__list--click');
+            }
+
+
+
+            
+
+        };
+        
     }
 
 }, false)
-
-
-
